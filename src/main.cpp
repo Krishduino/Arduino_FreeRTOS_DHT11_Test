851fdccf6c22b9a150c10b4feae4fc33abb766ea
Krishduino <38496983+Krishduino@users.noreply.github.com>
2021-01-14 20:11:58 +0200

    1st Commit, got the DHT to work.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,75 @@
 #include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
 #include <Adafruit_Sensor.h>
+#include <LiquidCrystal_I2C.h>
 #include <DHT.h>
-void setup() {
-  // put your setup code here, to run once:
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+float h;
+float t;
+
+static void meTask1(void *pvParameters);
+static void meTask2(void *pvParameters);
+static void idleTask(void *pvParameters);
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  xTaskCreate(meTask1, "Task1", 512, NULL, 1, NULL);
+  // xTaskCreate(meTask2, "Task2", 1000, NULL, 2, NULL);
+  xTaskCreate(idleTask, "Task0", 100, NULL, 0, NULL);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+static void meTask1(void *pvParameters)
+{
+  while (1)
+  {
+    (void)pvParameters;
+    dht.begin();
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    Serial.println("In Task1");
+    h = dht.readHumidity();
+    t = dht.readTemperature();
+    if (isnan(h))
+    {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+    }
+    Serial.println(h);
+    Serial.println(t);
+  }
+}
+
+// static void meTask2(void *pvParameters)
+
+// {
+//   while (1)
+//   {
+
+//     Serial.println("In Task2");
+//     vTaskDelay(2200 / portTICK_PERIOD_MS);
+//     Serial.print(F("Humidity: "));
+//     Serial.print(h);
+//     Serial.print(F("%  Temperature: "));
+//     Serial.print(t);
+//     Serial.print(F("°C "));
+//   }
+// }
+
+static void idleTask(void *pvParameters)
+{
+  while (1)
+  {
+    (void)pvParameters;
+    //Serial.println("In Idle now");
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+  }
+}
+
+void loop()
+{
 }
